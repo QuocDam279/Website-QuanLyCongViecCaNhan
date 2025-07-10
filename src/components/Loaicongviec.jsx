4d09ae24237e4a41438366550c4e70228e094259
QuocDam279 <quondamancy@gmail.com>
2025-07-10 10:29:55 +0700

    cài dat github action

--- src/components/Loaicongviec.jsx
+++ src/components/Loaicongviec.jsx
@@ -39,7 +39,7 @@ const Loaicongviec = () => {
   }
 
   return (
-    <div className="">
+    <div className="p-4">
       <div className="text-xl sm:text-2xl ml-2 mt-2 font-bold text-blue-700">
             LOẠI CÔNG VIỆC
       </div>

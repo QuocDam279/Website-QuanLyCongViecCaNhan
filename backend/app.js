fc2d5c5ff4f70f9843c8f42bce6a5f1544989a18
QuocDam279 <quondamancy@gmail.com>
2025-07-19 17:29:51 +0700

    Tích hơp API, thêm chưc năng lưu lich sư thay đôi

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+//backend/app.js
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
+const typejobRoutes = require('./routes/typejob.routes');
+const jobRoutes = require('./routes/job.routes');
+const logRoutes = require('./routes/log.routes');
+
+const app = express();
+dotenv.config();
+connectDB();
+
+const cors = require('cors');
+
+// Cho phép frontend 5173 gọi được
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/typejob', typejobRoutes);
+app.use('/api/job', jobRoutes); 
+app.use('/api/logs', logRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

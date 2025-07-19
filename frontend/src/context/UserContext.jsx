a8f0d19f67340666f644f493c075ef79eeb6be8b
QuocDam279 <quondamancy@gmail.com>
2025-07-19 17:48:41 +0700

    Tách file UserContext

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,21 +1,6 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { createContext } from 'react';
 
 export const UserContext = createContext({
   userName: '',
   setUserName: () => {},
 });
-
-export const UserProvider = ({ children }) => {
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    const storedName = localStorage.getItem('userName');
-    if (storedName) setUserName(storedName);
-  }, []);
-
-  return (
-    <UserContext.Provider value={{ userName, setUserName }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
